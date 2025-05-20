05382e7597b3716338935b48434c233f485a4626
Abdmater <65725386+Abdmater@users.noreply.github.com>
2025-05-20 17:18:03 +0300

    Update db.js2

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -6,7 +6,7 @@ export const connectDB = async () => {
   try {
     // console.log(process.env.MONGODB_URI);
     
-    const conn = await mongoose.connect("mongodb+srv://120180507mater:ckzyoopdQC6pS6Y2@chat.uecpdrr.mongodb.net/?retryWrites=true&w=majority&appName=chat/chat");
+    const conn = await mongoose.connect("mongodb+srv://120180507mater:4ybfa3v95Zda0o9T@cluster0.i7eng2d.mongodb.net/chat_db?retryWrites=true&w=majority&appName=Cluster0");
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
